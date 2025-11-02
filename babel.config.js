0a40f6ea44cfce0ff8b9f7425fdbddff3fa8a001
nmtien1801 <nickname1801@gmail.com>
2025-11-02 12:02:45 +0700

    fix bug v1

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,19 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ["module:metro-react-native-babel-preset"],
   plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env',
-      path: '.env',
-      blacklist: null,
-      whitelist: null,
-      safe: false,
-      allowUndefined: true,
-    }]
-  ]
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+    "react-native-reanimated/plugin",
+  ],
 };
