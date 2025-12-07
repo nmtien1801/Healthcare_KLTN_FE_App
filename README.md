5b30997dbecc63cbd67ee38ff2277883be713285
nmtien1801 <nickname1801@gmail.com>
2025-12-07 08:41:59 +0700

    done

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ EXPO_PUBLIC_URL_WEB=http://localhost:8080/api
 
 npx patch-package expo
 npx expo install --fix
+adb logcat *:S ReactNative:V ReactNativeJS:V
 
 <!-- build -->
 npx expo prebuild --clean
@@ -21,3 +22,7 @@ npx react-native start
 npx expo start --clear
 
 
+<!-- cài chạy release -->
+keytool -genkeypair -v -storetype PKCS12 -keystore my-upload-key.keystore -alias NguyenMinhTien -keyalg RSA -keysize 2048 -validity 10000
+
+keytool -list -v -keystore android/app/my-upload-key.keystore -alias NguyenMinhTien
--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,22 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    [
-      "module:react-native-dotenv",
-      {
-        moduleName: "@env",
-        path: ".env",
-        blacklist: null,
-        whitelist: null,
-        safe: false,
-        allowUndefined: true,
-      },
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ["module:babel-preset-expo"], 
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      ["@babel/plugin-transform-private-methods", { loose: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      "react-native-reanimated/plugin", 
     ],
-    ["@babel/plugin-transform-private-methods", { loose: true }],
-    ["@babel/plugin-proposal-class-properties", { loose: true }],
-    "react-native-reanimated/plugin",
-  ],
+  };
 };
